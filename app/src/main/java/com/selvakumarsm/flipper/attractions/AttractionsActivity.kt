80ac145d65d780e83415ec8e513a75181ea0c88c
Selvakumar Mohan <mailto.selvakumar.sm@gmail.com>
2020-09-21 17:31:25 +0530

    Created expaned page for attractions

---
+++ app/src/main/java/com/selvakumarsm/flipper/attractions/AttractionsActivity.kt
@@ -0,0 +1,12 @@
+package com.selvakumarsm.flipper.attractions
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.selvakumarsm.flipper.R
+
+class AttractionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_attractions)
+    }
+}
