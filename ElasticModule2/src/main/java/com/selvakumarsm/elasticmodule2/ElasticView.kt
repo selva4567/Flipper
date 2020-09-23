f8236da95cca12e4bb29f2be975d2e885b35cd06
Selvakumar Mohan <mailto.selvakumar.sm@gmail.com>
2020-09-23 12:31:04 +0530

    Remove the transition setting in custom motion class

--- ElasticModule2/src/main/java/com/selvakumarsm/elasticmodule2/ElasticView.kt
+++ ElasticModule2/src/main/java/com/selvakumarsm/elasticmodule2/ElasticView.kt
@@ -23,39 +23,20 @@ class ElasticView @JvmOverloads constructor(
 
     var state: State = State.EXPANDED
     lateinit var viewTag: String
-    private val collapsedSceneId: Int
-    private val expandedSceneId: Int
 
-    init {
-        val arr = context.obtainStyledAttributes(attrs, R.styleable.ElasticView, 0, 0)
-        collapsedSceneId = arr.getResourceId(R.styleable.ElasticView_collapsedSceneId, -1)
-        expandedSceneId = arr.getResourceId(R.styleable.ElasticView_expandedSceneId, -1)
-        arr.recycle()
-    }
-
-    internal fun expand() {
-        constraintSetIds.forEach {
-            Log.d(TAG, "expand: Constraint Id $it")
-        }
-        Log.d(TAG, "expand: $id to $expandedSceneId")
-        setTransition(collapsedSceneId, expandedSceneId)
-        setTransitionDuration(TRANSITION_DURATION)
+    fun expand() {
+        Log.d(TAG, "expand: $id")
         transitionToEnd()
         state = State.EXPANDED
     }
 
-    internal fun collapse() {
-        constraintSetIds.forEach {
-            Log.d(TAG, "collapse: Constraint Id $it")
-        }
-        Log.d(TAG, "collapse: $id to $collapsedSceneId")
-        setTransition(expandedSceneId, collapsedSceneId)
-        setTransitionDuration(TRANSITION_DURATION)
-        transitionToEnd()
+    fun collapse() {
+        Log.d(TAG, "collapse: $id ")
+        transitionToStart()
         state = State.COLLAPSED
     }
 
-    internal fun toggle() {
+    fun toggle() {
         when (state) {
             State.EXPANDED -> collapse()
             State.COLLAPSED -> expand()
