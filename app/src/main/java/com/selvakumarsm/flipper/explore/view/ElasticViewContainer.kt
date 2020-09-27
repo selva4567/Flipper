8f72ac5472773ab9035077604f4ae3d0a2131a62
Selvakumar Mohan <mailto.selvakumar.sm@gmail.com>
2020-09-27 20:12:02 +0530

    Expanding/collapsing view inside container smoothly

--- app/src/main/java/com/selvakumarsm/flipper/explore/view/ElasticViewContainer.kt
+++ app/src/main/java/com/selvakumarsm/flipper/explore/view/ElasticViewContainer.kt
@@ -11,8 +11,10 @@ import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.constraintlayout.motion.widget.MotionScene
 import androidx.constraintlayout.motion.widget.TransitionBuilder
 import androidx.constraintlayout.widget.ConstraintSet
-import androidx.core.view.marginTop
-import com.selvakumarsm.elasticmodule2.ElasticVerticalView
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.channels.consumesAll
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class ElasticViewContainer @JvmOverloads constructor(
     context: Context,
@@ -37,37 +39,95 @@ class ElasticViewContainer @JvmOverloads constructor(
         val layoutParams = LayoutParams(MATCH_PARENT, fromDp(context, 200)).also {
             it.topMargin = fromDp(context, 20)
         }
+        view.setOnClickListener {
+            Log.d(TAG, "addMotionView: VIew ${view.id} clicked")
+            toggle(it)
+        }
         addView(view, layoutParams)
         Log.d(TAG, "addMotionView: Total child count $childCount")
         applyConstraint(view)
+    }
+
+    private fun toggle(view: View) {
+        when (currentState) {
+            containerTransition!!.startConstraintSetId -> {
+                val startScene = getConstraintSet(containerTransition!!.startConstraintSetId)
+                startScene.clone(this)
+                val endScene = getConstraintSet(containerTransition!!.endConstraintSetId)
+                endScene.clone(this)
+                endScene.constrainWidth(view.id, MATCH_PARENT)
+                endScene.constrainHeight(view.id, WRAP_CONTENT)
+                endScene.connect(
+                    view.id,
+                    ConstraintSet.START,
+                    ConstraintSet.PARENT_ID,
+                    ConstraintSet.START,
+                    fromDp(context, 20)
+                )
+                endScene.connect(
+                    view.id,
+                    ConstraintSet.TOP,
+                    ConstraintSet.PARENT_ID,
+                    ConstraintSet.TOP,
+                    fromDp(context, 20)
+                )
+                endScene.connect(
+                    view.id,
+                    ConstraintSet.END,
+                    ConstraintSet.PARENT_ID,
+                    ConstraintSet.END,
+                    fromDp(context, 20)
+                )
+                endScene.connect(
+                    view.id,
+                    ConstraintSet.BOTTOM,
+                    ConstraintSet.PARENT_ID,
+                    ConstraintSet.BOTTOM,
+                    fromDp(context, 20)
+                )
+                setTransition(
+                    containerTransition!!.startConstraintSetId,
+                    containerTransition!!.endConstraintSetId
+                )
+                transitionToEnd()
+            }
+            containerTransition!!.endConstraintSetId -> {
+//                applyConstraint(view)
+                transitionToStart()
+            }
+            else -> {
+                Log.w(TAG, "toggle: Unknown current state $currentState")
+                return
+            }
+        }
+
     }
 
     private fun applyConstraint(child: View) {
         TransitionManager.beginDelayedTransition(this)
         val childBefore = if (childCount <= 1) null else getChildAt(childCount - 2)
         Log.d(TAG, "applyConstraint: aligning ${child.id} vertically to ${childBefore?.id}")
-        val constraintSet = ConstraintSet()
-        constraintSet.clone(this)
-        constraintSet.connect(
+        val startScene = getConstraintSet(containerTransition!!.startConstraintSetId)
+        startScene.clone(this)
+        startScene.connect(
             child.id,
             ConstraintSet.TOP,
             childBefore?.id ?: ConstraintSet.PARENT_ID,
             if (childBefore == null) ConstraintSet.TOP else ConstraintSet.BOTTOM
         )
-        constraintSet.connect(
+        startScene.connect(
             child.id,
             ConstraintSet.START,
             ConstraintSet.PARENT_ID,
             ConstraintSet.START
         )
-        constraintSet.connect(
+        startScene.connect(
             child.id,
             ConstraintSet.END,
             ConstraintSet.PARENT_ID,
             ConstraintSet.END
         )
-        constraintSet.applyTo(this)
-        getConstraintSet(containerTransition!!.startConstraintSetId).also { it.clone(this) }
+        startScene.applyTo(this)
         setTransition(
             containerTransition!!.startConstraintSetId,
             containerTransition!!.endConstraintSetId
